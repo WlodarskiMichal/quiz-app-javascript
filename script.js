988d2922fd49db926b8d6d8d45907735eb221b90
howmehow <howmehow@gmail.com>
2021-03-25 13:31:52 +0100

    Adding questions to the quiz

---
+++ script.js
@@ -0,0 +1,36 @@
+const quizData = [
+    {
+        question: "Which language is most popular in Web development?",
+        a: "Java",
+        b: "C#",
+        c: "Pyhon",
+        d: "JavaScript",
+        correct: "d",
+}
+]
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+
+}
+
